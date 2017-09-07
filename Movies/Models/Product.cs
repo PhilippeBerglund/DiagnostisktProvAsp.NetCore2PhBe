b26ba51f5c2bb061afcbeadba2614dda398bddea
Philippe Berglund <philippe05@gmail.com>
2017-09-07 10:14:36 +0200

    - Lägg till valideringsattribut på Name (Required) och Price (Range mellan 5 och 1000).   Gör en commit.

--- Movies/Models/Product.cs
+++ Movies/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,10 @@ namespace Movies.Models
     public class Product
     {
         public int ProductId { get; set; }
+        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
+        [Range(5, 1000, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
         public decimal Price { get; set; }
+
     }
 }
